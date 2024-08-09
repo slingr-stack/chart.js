746b512004025d6c3ba5eab571ccca549aa9a3a0
Sthefano Politino <stouma@slingr.io>
2024-08-09 16:45:57 -0300

    test lib

---
+++ helpers.js
@@ -0,0 +1,733 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "HALF_PI", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.H;
+  }
+});
+Object.defineProperty(exports, "INFINITY", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b2;
+  }
+});
+Object.defineProperty(exports, "PI", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.P;
+  }
+});
+Object.defineProperty(exports, "PITAU", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b1;
+  }
+});
+Object.defineProperty(exports, "QUARTER_PI", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b4;
+  }
+});
+Object.defineProperty(exports, "RAD_PER_DEG", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b3;
+  }
+});
+Object.defineProperty(exports, "TAU", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.T;
+  }
+});
+Object.defineProperty(exports, "TWO_THIRDS_PI", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b5;
+  }
+});
+Object.defineProperty(exports, "_addGrace", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.R;
+  }
+});
+Object.defineProperty(exports, "_alignPixel", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.X;
+  }
+});
+Object.defineProperty(exports, "_alignStartEnd", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a2;
+  }
+});
+Object.defineProperty(exports, "_angleBetween", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.p;
+  }
+});
+Object.defineProperty(exports, "_angleDiff", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b6;
+  }
+});
+Object.defineProperty(exports, "_arrayUnique", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment._;
+  }
+});
+Object.defineProperty(exports, "_attachContext", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a8;
+  }
+});
+Object.defineProperty(exports, "_bezierCurveTo", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.as;
+  }
+});
+Object.defineProperty(exports, "_bezierInterpolation", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ap;
+  }
+});
+Object.defineProperty(exports, "_boundSegment", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ax;
+  }
+});
+Object.defineProperty(exports, "_boundSegments", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.an;
+  }
+});
+Object.defineProperty(exports, "_capitalize", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a5;
+  }
+});
+Object.defineProperty(exports, "_computeSegments", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.am;
+  }
+});
+Object.defineProperty(exports, "_createResolver", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a9;
+  }
+});
+Object.defineProperty(exports, "_decimalPlaces", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aK;
+  }
+});
+Object.defineProperty(exports, "_deprecated", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aV;
+  }
+});
+Object.defineProperty(exports, "_descriptors", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aa;
+  }
+});
+Object.defineProperty(exports, "_elementsEqual", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ah;
+  }
+});
+Object.defineProperty(exports, "_factorize", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.N;
+  }
+});
+Object.defineProperty(exports, "_filterBetween", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aO;
+  }
+});
+Object.defineProperty(exports, "_getParentNode", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.I;
+  }
+});
+Object.defineProperty(exports, "_getStartAndCountOfVisiblePoints", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.q;
+  }
+});
+Object.defineProperty(exports, "_int16Range", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.W;
+  }
+});
+Object.defineProperty(exports, "_isBetween", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aj;
+  }
+});
+Object.defineProperty(exports, "_isClickEvent", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ai;
+  }
+});
+Object.defineProperty(exports, "_isDomSupported", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.M;
+  }
+});
+Object.defineProperty(exports, "_isPointInArea", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.C;
+  }
+});
+Object.defineProperty(exports, "_limitValue", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.S;
+  }
+});
+Object.defineProperty(exports, "_longestText", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aN;
+  }
+});
+Object.defineProperty(exports, "_lookup", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aP;
+  }
+});
+Object.defineProperty(exports, "_lookupByKey", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.B;
+  }
+});
+Object.defineProperty(exports, "_measureText", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.V;
+  }
+});
+Object.defineProperty(exports, "_merger", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aT;
+  }
+});
+Object.defineProperty(exports, "_mergerIf", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aU;
+  }
+});
+Object.defineProperty(exports, "_normalizeAngle", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ay;
+  }
+});
+Object.defineProperty(exports, "_parseObjectDataRadialScale", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.y;
+  }
+});
+Object.defineProperty(exports, "_pointInLine", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aq;
+  }
+});
+Object.defineProperty(exports, "_readValueToProps", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ak;
+  }
+});
+Object.defineProperty(exports, "_rlookupByKey", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.A;
+  }
+});
+Object.defineProperty(exports, "_scaleRangesChanged", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.w;
+  }
+});
+Object.defineProperty(exports, "_setMinAndMaxByKey", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aG;
+  }
+});
+Object.defineProperty(exports, "_splitKey", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aW;
+  }
+});
+Object.defineProperty(exports, "_steppedInterpolation", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ao;
+  }
+});
+Object.defineProperty(exports, "_steppedLineTo", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ar;
+  }
+});
+Object.defineProperty(exports, "_textX", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aB;
+  }
+});
+Object.defineProperty(exports, "_toLeftRightCenter", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a1;
+  }
+});
+Object.defineProperty(exports, "_updateBezierControlPoints", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.al;
+  }
+});
+Object.defineProperty(exports, "addRoundedRectPath", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.au;
+  }
+});
+Object.defineProperty(exports, "almostEquals", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aJ;
+  }
+});
+Object.defineProperty(exports, "almostWhole", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aI;
+  }
+});
+Object.defineProperty(exports, "callback", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.Q;
+  }
+});
+Object.defineProperty(exports, "clearCanvas", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.af;
+  }
+});
+Object.defineProperty(exports, "clipArea", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.Y;
+  }
+});
+Object.defineProperty(exports, "clone", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aS;
+  }
+});
+Object.defineProperty(exports, "color", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.c;
+  }
+});
+Object.defineProperty(exports, "createContext", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.j;
+  }
+});
+Object.defineProperty(exports, "debounce", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ad;
+  }
+});
+Object.defineProperty(exports, "defined", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.h;
+  }
+});
+Object.defineProperty(exports, "distanceBetweenPoints", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aE;
+  }
+});
+Object.defineProperty(exports, "drawPoint", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.at;
+  }
+});
+Object.defineProperty(exports, "drawPointLegend", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aD;
+  }
+});
+Object.defineProperty(exports, "each", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.F;
+  }
+});
+Object.defineProperty(exports, "easingEffects", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.e;
+  }
+});
+Object.defineProperty(exports, "finiteOrDefault", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.O;
+  }
+});
+Object.defineProperty(exports, "fontString", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a$;
+  }
+});
+Object.defineProperty(exports, "formatNumber", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.o;
+  }
+});
+Object.defineProperty(exports, "getAngleFromPoint", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.D;
+  }
+});
+Object.defineProperty(exports, "getHoverColor", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aR;
+  }
+});
+Object.defineProperty(exports, "getMaximumSize", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.G;
+  }
+});
+Object.defineProperty(exports, "getRelativePosition", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.z;
+  }
+});
+Object.defineProperty(exports, "getRtlAdapter", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.az;
+  }
+});
+Object.defineProperty(exports, "getStyle", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a_;
+  }
+});
+Object.defineProperty(exports, "isArray", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b;
+  }
+});
+Object.defineProperty(exports, "isFinite", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.g;
+  }
+});
+Object.defineProperty(exports, "isFunction", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a7;
+  }
+});
+Object.defineProperty(exports, "isNullOrUndef", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.k;
+  }
+});
+Object.defineProperty(exports, "isNumber", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.x;
+  }
+});
+Object.defineProperty(exports, "isObject", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.i;
+  }
+});
+Object.defineProperty(exports, "isPatternOrGradient", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aQ;
+  }
+});
+Object.defineProperty(exports, "listenArrayEvents", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.l;
+  }
+});
+Object.defineProperty(exports, "log10", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aM;
+  }
+});
+Object.defineProperty(exports, "merge", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a4;
+  }
+});
+Object.defineProperty(exports, "mergeIf", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ab;
+  }
+});
+Object.defineProperty(exports, "niceNum", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aH;
+  }
+});
+Object.defineProperty(exports, "noop", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aF;
+  }
+});
+Object.defineProperty(exports, "overrideTextDirection", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aA;
+  }
+});
+Object.defineProperty(exports, "readUsedSize", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.J;
+  }
+});
+Object.defineProperty(exports, "renderText", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.Z;
+  }
+});
+Object.defineProperty(exports, "requestAnimFrame", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.r;
+  }
+});
+Object.defineProperty(exports, "resolve", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a;
+  }
+});
+Object.defineProperty(exports, "resolveObjectKey", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.f;
+  }
+});
+Object.defineProperty(exports, "restoreTextDirection", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aC;
+  }
+});
+Object.defineProperty(exports, "retinaScale", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ae;
+  }
+});
+Object.defineProperty(exports, "setsEqual", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ag;
+  }
+});
+Object.defineProperty(exports, "sign", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.s;
+  }
+});
+Object.defineProperty(exports, "splineCurve", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aY;
+  }
+});
+Object.defineProperty(exports, "splineCurveMonotone", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aZ;
+  }
+});
+Object.defineProperty(exports, "supportsEventListenerOptions", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.K;
+  }
+});
+Object.defineProperty(exports, "throttled", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.L;
+  }
+});
+Object.defineProperty(exports, "toDegrees", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.U;
+  }
+});
+Object.defineProperty(exports, "toDimension", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.n;
+  }
+});
+Object.defineProperty(exports, "toFont", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.a0;
+  }
+});
+Object.defineProperty(exports, "toFontString", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aX;
+  }
+});
+Object.defineProperty(exports, "toLineHeight", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.b0;
+  }
+});
+Object.defineProperty(exports, "toPadding", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.E;
+  }
+});
+Object.defineProperty(exports, "toPercentage", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.m;
+  }
+});
+Object.defineProperty(exports, "toRadians", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.t;
+  }
+});
+Object.defineProperty(exports, "toTRBL", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.av;
+  }
+});
+Object.defineProperty(exports, "toTRBLCorners", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.aw;
+  }
+});
+Object.defineProperty(exports, "uid", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.ac;
+  }
+});
+Object.defineProperty(exports, "unclipArea", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.$;
+  }
+});
+Object.defineProperty(exports, "unlistenArrayEvents", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.u;
+  }
+});
+Object.defineProperty(exports, "valueOrDefault", {
+  enumerable: true,
+  get: function get() {
+    return _helpersSegment.v;
+  }
+});
+var _helpersSegment = require("./chunks/helpers.segment.js");
+require("@kurkle/color");
